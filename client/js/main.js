bb5b3478674f2862dd162e18222474023f916bca
Sensational Code <sensational.code@gmail.com>
2018-09-06 19:50:11 -0700

    Rough replacement of ViewManager for Vue.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,44 +1,61 @@
-var viewManager = new ViewManager();
-
 window.addEventListener('load', function(event) {
 	init();
 });
 
 function init() {
-	var game = new SnakeGame();
+
+	var app = new Vue({
+		el: '#app',
+		data: {
+			page: 'home-view',
+			playerName: null,
+			players: [],
+			joinLobbyText: ''
+		},
+		methods: {
+			joinAnyLobby: function() {
+				var self = this;
+				socket.emit('join-any-lobby', {});
+				socket.on('found-lobby', function(id) {
+					self.joinLobby(id);
+				});
+			},
+
+			createLobby: function() {
+				var playerName = this.playerName;
+				socket.emit('create-lobby', {
+					playerName: playerName
+				});
+				onLobbyJoined();
+			},
+
+			joinLobby: function(id) {
+				var playerName = this.playerName;
+				socket.emit('join-lobby', {
+					playerName: playerName,
+					id: id
+				});
+				onLobbyJoined();
+			},
+			
+			updateLobbyData: function(data) {
+				this.players = data.players;
+			},
+
+			startLobby: function() {
+				socket.emit('lobby-start');
+			}
+	  }
+	});
+
 	var socket = io.connect();
+	var game = new SnakeGame();
 
 	var urlParams = helpers.getAllUrlParams(window.location.href);
 	var lobbyId = urlParams.lobby;
 
-	viewManager.init();
-
 	if (lobbyId) {
-		joinLobby(lobbyId);
-	}
-
-	function joinAnyLobby() {
-		socket.emit('join-any-lobby', {});
-		socket.on('found-lobby', function(id) {
-			joinLobby(id);
-		});
-	}
-
-	function createLobby() {
-		var playerName = viewManager.playerName.value;
-		socket.emit('create-lobby', {
-			playerName: playerName
-		});
-		onLobbyJoined();
-	}
-
-	function joinLobby(id) {
-		var playerName = viewManager.playerName.value;
-		socket.emit('join-lobby', {
-			playerName: playerName,
-			id: id
-		});
-		onLobbyJoined();
+		app.joinLobby(lobbyId);
 	}
 
 	function onLobbyJoined() {
@@ -54,28 +71,23 @@ function init() {
 
 		socket.on('update-game', function(data) {
 			console.log('update game')
-			viewManager.updateLobbyData(data);
+			app.updateLobbyData(data);
 			game.updateData(data);
 			if (data.players[socket.id].isHost) {
-				viewManager.startGameButton.disabled = false;
+				app.$refs.startGameButton.disabled = false;
 			}
 		});
 	}
 
-	viewManager.playButton.onclick = joinAnyLobby;
-	viewManager.createButton.onclick = createLobby;
-	viewManager.joinButton.onclick = function(event) {
-		joinLobby(viewManager.joinInput.value);
-	}
-
 	function handleGameStart(data) {
 		console.log('Game start!');
-		viewManager.showGameView();
+		app.page = 'game-view';
 		game.init(socket);
 	}
+
 	function handleGameEnd(data) {
 		console.log('Game end!');
-		viewManager.showLobbyView();
+		app.page = 'lobby-view';
 	}
 
 	function handleLobbyJoined(data) {
@@ -85,16 +97,14 @@ function init() {
 
 		// if the game is in progress, just add this player straight into to the game
 		if (data.inGame) {
-			viewManager.showGameView();
+			app.page = 'game-view';
 			game.init(socket);
 		}
 		else {
-			viewManager.showLobbyView();
-			viewManager.updateLobbyData(data);
-		}
-
-		viewManager.startGameButton.onclick = function() {
-			socket.emit('lobby-start');
+			app.page = 'lobby-view';
+			Vue.nextTick(function () {
+				app.updateLobbyData(data);
+			});
 		}
 
 		socket.on('game-start', handleGameStart);
