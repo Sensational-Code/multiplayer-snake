70f45c74c031f3cd9e122f0ace4f05e12f027b4b
Sensational Code <sensational.code@gmail.com>
2019-03-31 12:58:49 -0700

    Generate a random name for unnamed players instead of just "Guest"

--- server/player.js
+++ server/player.js
@@ -1,9 +1,11 @@
 const randomColor = require('randomcolor');
+const capitalize = require('capitalize');
+const nameGenerator = require('project-name-generator');
 
 class Player {
 	constructor(id, name) {
 		this.id = id;
-		this.name = name || 'Guest';
+		this.name = name || capitalize.words(nameGenerator().spaced);
 		this.isHost = false;
 		this.blocks = [{ x: 0, y: 0 }];
 		this.length = 1;
